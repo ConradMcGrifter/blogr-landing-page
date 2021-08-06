709c204fe77af17590c8ec6d68ee6bb5fd91aca6
skf <79630413+ConradMcGrifter@users.noreply.github.com>
2021-08-06 00:44:43 -0400

    fixed issue with dropdown-content and menubutton styles still being active after closing and reopening mobile menu

--- js/navBar.js
+++ js/navBar.js
@@ -1,6 +1,14 @@
 const HAMBURGER = document.getElementById("hamburger--btn");
 const menuContainer = document.querySelector(".menu--container");
+const dropdownContent = document.querySelectorAll(".dropdown-content");
+const menuButton = document.querySelectorAll(".menu-button");
 
 HAMBURGER.addEventListener("click", () => {
     menuContainer.classList.toggle("display");
+
+    for (let i = 0; i < dropdownContent.length; i++) {
+        dropdownContent[i].classList.remove("display");
+        menuButton[i].classList.remove("active");
+        menuButton[i].classList.remove("rotate");
+    }
 });

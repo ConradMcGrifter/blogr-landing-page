a17989c3a810b2772f7df85840704f740ffcbc27
skf <79630413+ConradMcGrifter@users.noreply.github.com>
2021-08-06 14:39:35 -0400

    update js
    added some comments

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,7 +1,8 @@
 const menuButton = document.querySelectorAll(".menu-button");
 const dropdownContent = document.querySelectorAll(".dropdown-content");
 
-const setClass = (ele, className) => {
+// this function takes an array of elements and a class name as parameters then loops through the array and removes the classname from the elements
+const removeClass = (ele, className) => {
     for (let i = 0; i < ele.length; i++) {
         ele[i].classList.remove(className);
     }
@@ -9,12 +10,15 @@ const setClass = (ele, className) => {
 
 for (let i = 0; i < menuButton.length; i++) {
     menuButton[i].addEventListener("click", function () {
+        // checks if the dropdown content is hidden and stores true or false result in the variable
         let setClasses = !dropdownContent[i].classList.contains("display");
 
-        setClass(dropdownContent, "display");
-        setClass(menuButton, "rotate");
-        setClass(menuButton, "active");
+        // cycle through the element arrays and remove the targeted class from all elements
+        removeClass(dropdownContent, "display");
+        removeClass(menuButton, "rotate");
+        removeClass(menuButton, "active");
 
+        //this code runs if the dropdownContent was hidden when the button is first clicked
         if (setClasses) {
             dropdownContent[i].classList.add("display");
             menuButton[i].classList.add("rotate");
--- js/navBar.js
+++ js/navBar.js
@@ -6,6 +6,7 @@ const menuButton = document.querySelectorAll(".menu-button");
 HAMBURGER.addEventListener("click", () => {
     menuContainer.classList.toggle("display");
 
+    // this loops through the dropdownContent / menuButton elements and removes the active state classes so that if one of the dropdownContent are showing when the hamburger is clicked to close the menu, it will reset the menu display to closed.
     for (let i = 0; i < dropdownContent.length; i++) {
         dropdownContent[i].classList.remove("display");
         menuButton[i].classList.remove("active");
